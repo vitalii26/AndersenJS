5e8d110ed5eb6047f05ea6f0d97699acd4b1273f
Vitalii <vitalik77795@ukr.net>
2021-07-08 14:30:59 +0300

    lesson_1_problems_fix

--- lesson_1/index.js
+++ lesson_1/index.js
@@ -1,25 +1,35 @@
-const checkToNumber = value => isFinite(value) && value !== '' && value !== null;
+const errorMessage = 'Некорректный ввод!';
+const isNumber = value => isFinite(value) && value !== '' && value !== null;
 const calcToNumberSystem = (number, base) => parseInt(number, 10).toString(base);
 
-const firstValue = prompt('Введите число', '');
-const radix = prompt('Введите второе число(система счисления)', '');
+function printToNumberSystem() {
+    const MIN_NUMBER_BASE = 2;
+    const MAX_NUMBER_BASE = 36;
+    const firstValue = prompt('Введите число', '');
+    const radix = prompt('Введите второе число(система счисления)', '');
 
-if (checkToNumber(firstValue) && checkToNumber(radix) && radix >=2 && radix <= 36) {
-    console.log(calcToNumberSystem(firstValue, radix));
-} else {
-    console.log('Некорректный ввод!');
-}
-
-// 2
-const firstNum = prompt('Введите первое число', '');
-
-if (checkToNumber(firstNum)) {
-    const secondNum = prompt('Введите второе число', '');
-    if (checkToNumber(secondNum)) {
-        console.log(`Ответ: ${+firstNum + Number(secondNum)}, ${Math.round(firstNum / secondNum)}`);
-    } else {
-        console.log('Некорректный ввод!');
+    if (isNumber(firstValue) && isNumber(radix) && radix >= MIN_NUMBER_BASE && radix <= MAX_NUMBER_BASE) {
+        console.log(calcToNumberSystem(firstValue, radix));
+        return;
     }
-} else {
-    console.log('Некорректный ввод!');
+    console.log(errorMessage);
 }
+
+function printSumAndDivision() {
+    const firstNum = prompt('Введите первое число', '');
+
+    if (isNumber(firstNum)) {
+        const secondNum = prompt('Введите второе число', '');
+        if (isNumber(secondNum)) {
+            console.log(`Ответ: ${Number(firstNum) + Number(secondNum)}, ${Math.round(firstNum / secondNum)}`);
+        } else {
+            console.log(errorMessage);
+        }
+    } else {
+        console.log(errorMessage);
+    }
+}
+
+printToNumberSystem();
+printSumAndDivision();
+
